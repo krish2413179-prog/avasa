6c964edd6af1b2c9b6ad8e6f20beefc42b498ca6
krish2413179-prog <krish2413179@akgec.ac.in>
2025-12-25 15:29:00 +0530

    Final Envio indexer fix: Clean up old generated files, indexer now running successfully
     Removed old generated files with TestToken references
     Envio indexer running on WSL with GraphQL endpoint active
     All RWA property contracts being indexed
     Real-time portfolio tracking operational
     GraphQL endpoint: http://localhost:8080/v1/graphql

--- generated/src/db/Enums.gen.ts
+++
@@ -1,8 +0,0 @@
-/* TypeScript file generated from Enums.res by genType. */
-
-/* eslint-disable */
-/* tslint:disable */
-
-export type ContractType_t = "TestToken";
-
-export type EntityType_t = "Transfer" | "dynamic_contract_registry";
